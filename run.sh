c0e2a612ff4e91f52ff09b50bd4ab8e3e78b3be6
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-11-13 13:45:05 +0000

    put each uwsgi-managed app in vassal subdirectory
    update uwsgi emperor to use glob monitor pattern https://uwsgi-docs.readthedocs.io/en/latest/ImperialMonitors.html#glob-monitor-a-shell-pattern

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-uwsgi --master --fastrouter 127.0.0.1:3017 --fastrouter-subscription-server 127.0.0.1:7000 --emperor ./vassals
+uwsgi --master --fastrouter 127.0.0.1:3017 --fastrouter-subscription-server 127.0.0.1:7000 --emperor ./vassals/*/*.ini
 
